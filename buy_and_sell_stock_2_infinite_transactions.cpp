efce4f9568959abda68ce19a426d47139ed9c24f
Satyam-Muluk <73930297+Satyam-Muluk@users.noreply.github.com>
2022-04-05 13:10:26 +0530

    added buy and sell stock II with infinite transactions

---
+++ buy_and_sell_stock_2_infinite_transactions.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int maxProfit(vector<int> &prices,int index,int buyornot,vector<vector<int>> &dp)
+{
+    if(index==prices.size())
+    {
+        return 0;
+    }
+    if(dp[index][buyornot]!=-1)
+       return dp[index][buyornot];
+    int profit = 0;
+    if(buyornot)
+    {
+        int buy = -prices[index]+maxProfit(prices,index+1,0,dp);
+        int notbuy = maxProfit(prices,index+1,1,dp);
+        profit = max(buy,notbuy);
+    }
+    else
+    {
+        int sell = prices[index]+maxProfit(prices,index+1,1,dp);
+        int notsell = maxProfit(prices,index+1,0,dp);
+        profit = max(sell,notsell);
+    }
+    return dp[index][buyornot] = profit;
+}
+int main()
+{
+    vector<int> prices = {7,1,5,3,6,4};
+    vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
+    cout<<maxProfit(prices,0,1,dp);
+}
